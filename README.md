c1b2cd932d213320bdf3a26ddc3b9dc6f5838778
whit <whitspa64@gmail.com>
2022-10-17 23:33:47 -0400

    Third Commit. created If The statements to validate user character choices. Created fcn to randomly selct charatcer choices and write them to pword

--- README.md
+++ README.md
@@ -4,14 +4,16 @@ Password Generator.
 I worked mainly with Javascript to build this solution.
 My comments are entirely in the Javscript file.
 Here is the link to the deployed application:
-pending
+https://whitspa.github.io/challenge-03-password-generator/
 
 And here is the link to the repository:
-pending
+https://github.com/whitspa/challenge-03-password-generator.git
 
 My github repository is published under the MIT license
 and I can be reached via email at: whitspa64@gmail.com
 with any questions.
 
-A screen shot of the deployed portfolio is below
-pendingthis is where I store my code for the password generator challenge
+A screen shot of the deployed application is below
+<img src="password-generator-screenshot.png"
+     alt="Password Generator Prompt Box"
+     style="float: left; margin-right: 10px;" />
--- script.js
+++ script.js
@@ -2,15 +2,6 @@
 // Get references to the #generate element
 const generateBtn = document.querySelector("#generate");
 
-function writePassword() { 
-    const password = generatePassword();
-    const passwordText = document.querySelector("#password");
-    passwordText.value = password;
-  }
-  
-// Add event listener to generate button
-  generateBtn.addEventListener("click", writePassword);
-
 function promptTypes(){ ////Prompt user for password options password length, lower/upper case, numbers, special characters
   var passwordMaker = []
 const chooseSpecChar = confirm("Click OK if you want to include symbols");
@@ -34,3 +25,49 @@ if (chooseLowerCase) {
   const lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","w","x","y","z"];
   passwordMaker = passwordMaker.concat(lowerCase);
 }
+if(!passwordMaker.length){ //validate the chracter type choice and prompt user again if no character type choice is made
+  alert("You must select at least on character type")
+  var redo = confirm("Want to select again?")
+  console.log(redo)
+  if(!redo) {return null}
+  return promptTypes()
+}
+return passwordMaker
+}
+
+function generatePassword(){
+
+const passwordLength = prompt ("Choose a password length of at least 8 characters and no more than 128 chsracters.");
+console.log("password length: ", passwordLength); //Prompt user for a choice of password length 
+if (passwordLength === null){ //validate user choice of password length. if no password length choice, go back to password length prompt
+  return
+}
+if ( passwordLength < 8 || passwordLength > 128) {// if password length outside of parameters, go back to password length prompt
+  alert ("Password Length Incorrect.")
+  return generatePassword()
+}
+
+let passwordMaker = promptTypes() //concatinate the chosen character type/s
+
+if(!passwordMaker){
+  return
+}
+
+let password = "";
+for (i = 0; i < passwordLength; i++) { //pull characters at random out of the selected character type/s and write them to password
+  const num =Math.floor(Math.random() * passwordMaker.length);
+  const char = passwordMaker[num]
+  password += char
+}
+
+return password
+}
+//I moved this starter code (below) to the end since these are the last activities called by the solution
+function writePassword() { 
+  const password = generatePassword();
+  const passwordText = document.querySelector("#password");
+  passwordText.value = password;
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
